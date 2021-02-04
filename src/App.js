8eab755b4c0e6273d70d1182accdbbb218419566
Kaung Shein <nicholasling1412@gmail.com>
2021-02-04 14:49:29 -0500

    App.js - added axios get

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import React from "react";
+import axios from "axios";
 
 import Display from "./components/Display";
 import Form from "./components/Form";
 
 function App() {
+  // Url variable
+  const url = "http://localhost:3000";
+
+  // States
+  const [songs, setSongs] = React.useState([]);
+
+  // Function to get list of songs
+  const getSongs = () => {
+    axios.get(url + "/tunatunr").then((response) => {
+      setSongs(response.data);
+    });
+  };
+
+  React.useEffect(() => {
+    getSongs();
+  }, []);
+
   return (
     <div className="App">
       <Display />
