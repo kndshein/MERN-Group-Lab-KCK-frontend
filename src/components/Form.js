1b30330a1f7573781a27f106fffa8d910facdf66
Kaung Shein <nicholasling1412@gmail.com>
2021-02-04 12:14:47 -0500

    Form - Finished rough draft of Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,12 @@ const Form = (props) => {
   return (
     <div className="add-a-song">
       <h3>Add a song. No fishy business.</h3>
+      <form>
+        <input type="text" name="title" />
+        <input type="text" name="artist" />
+        <input type="text" name="time" />
+        <input type="submit" value="Krillin' It" />
+      </form>
     </div>
   );
 };
